3ed047a747f1a21f745780394009dc312f4111b9
AllanPessin <allan.pessin@outlook.com>
2025-02-28 17:56:41 -0300

    feat: card de personagens retornando na página inicial com fução buscando 6 ids aleatorios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,33 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { Character, CharacterService } from '../../service/character/character.service';
 
 @Component({
     selector: 'app-home',
-    imports: [],
+    imports: [CardComponent, NgFor],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+    characters: Character[] = [];
+
+    constructor(private characterService: CharacterService) {}
+
+    ngOnInit(): void {
+        this.characterService.getMultipleCharacter(this.listIdGenerate()).subscribe({
+            next: (response: Character[]): void => {
+                console.log(response);
+                this.characters = response;
+            },
+        });
+    }
+
+    listIdGenerate() {
+        let num = new Set();
+        while (num.size < 6) {
+            num.add(Math.floor(Math.random() * 826) + 1);
+        }
+        return Array.from(num).join(',');
+    }
+}
