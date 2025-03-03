25a0d6d8228655e5685702b18950bd7f7aec7a81
AllanPessin <allan.pessin@outlook.com>
2025-03-03 09:53:33 -0300

    feat: menu mobile adicionado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { NavLinkComponent } from '../nav-link/nav-link.component';
 
 @Component({
     selector: 'app-header',
-    imports: [RouterModule, NavLinkComponent],
+    imports: [RouterModule, NavLinkComponent, FontAwesomeModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+    faBars = faBars;
+    faTimes = faTimes;
     imagePath: string = '/assets/images/logo.png';
+    isMenuOpen: boolean = false;
 
     navLinks: Array<{ label: string; link: string }> = [
         { label: 'Personagens', link: '/personagens' },
         { label: 'Localizações', link: '/localizacoes' },
         { label: 'Episódios', link: '/episodios' },
     ];
+
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+    }
 }
