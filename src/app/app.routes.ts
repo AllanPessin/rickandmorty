83f3a90be4088ee8a094603f8b947d26ab0762b8
AllanPessin <allan.pessin@outlook.com>
2025-03-05 21:49:26 -0300

    feat: rota de localizacoes filtradas adicionada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CharacterDetailComponent } from './pages/character-detail/character-detail.component';
 import { CharacterListComponent } from './pages/character-list/character-list.component';
-import { HomeComponent } from './pages/home/home.component';
-import { EpisodeListComponent } from './pages/episode-list/episode-list.component';
 import { EpisodeDetailComponent } from './pages/episode-detail/episode-detail.component';
+import { EpisodeListComponent } from './pages/episode-list/episode-list.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LocationDateilsComponent } from './pages/location-dateils/location-dateils.component';
+import { LocationListComponent } from './pages/location-list/location-list.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -11,6 +13,7 @@ export const routes: Routes = [
     { path: 'personagens/:id', component: CharacterDetailComponent },
     { path: 'episodios', component: EpisodeListComponent },
     { path: 'episodios/:id', component: EpisodeDetailComponent },
-    { path: 'localizacoes', component: CharacterListComponent },
+    { path: 'localizacoes', component: LocationListComponent },
+    { path: 'localizacoes/:id', component: LocationDateilsComponent },
     { path: '**', redirectTo: '/' },
 ];
